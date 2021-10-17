dfbb57d8132345ba732c69638b251cc036ac7895
murarijha123 <murari.jha@kelltontech.com>
2021-10-17 14:48:18 +0530

    Add project files.

---
+++ DemoProject/Model/Employee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DemoProject.Model
+{
+    public class Employee
+    {
+        public string Name { get; set; }
+
+        public string Adress { get; set; }
+
+        public int Rollno { get; set; }
+
+        public string Class { get; set; }
+    }
+}
